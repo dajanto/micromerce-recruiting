45cb4cb2c08bca84b055d9b2021157843d2f82f3
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2023-05-02 01:54:58 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,20 @@ type Dates = {
   endDate: string;
 };
 
-const extractDates = (customer: any) => {
-  // your code here
+const extractDates = (customer: any): Dates | [] => {
+  //const periodString = customer.ListOfPeriods.trim();
+  let periodString = null;
+  if (periodString == null) {
+    return [];
+  }
+
+  const startDate = '11.11.2008';
+  const endDate = '17.12.2008';
+
+  return {
+    startDate,
+    endDate,
+  };
 };
 
 export default extractDates;
