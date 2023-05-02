d97a991fad9aa4626a1d15e537e8b3224ead4f45
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2023-05-02 04:22:58 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,30 @@
 type Dates = {
-  startDate: string;
-  endDate: string;
+    startDate: string;
+    endDate: string;
 };
 
 function dashdivide(dashstring): Dates {
-  let delimiterdash = dashstring.trim().split("-");
-  let startDate = delimiterdash[0].trim();
-  let endDate = delimiterdash[1].trim();
-  return {startDate,endDate};
+    let delimiterdash = dashstring.trim().split("-");
+    let startDate = delimiterdash[0].trim();
+    let endDate = delimiterdash[1].trim();
+    return {startDate, endDate};
 }
 
-function strichdivide(strichstring): Dates[] {
-  return strichstring
-      .trim()
-      .split("|")
-      .map((date) => dashdivide(date));
+function strichdivide(strichstring) {
+    return strichstring
+        .trim()
+        .split("|")
+        .map((date) => dashdivide(date));
 }
-
 const extractDates = (customer: any): Dates[] | [] => {
-  if (
-      customer.ListOfPeriods == null ||
-      Object.hasOwn(customer, "ListOfPeriods")
-  ) {
-    return [];
-  }
+    if (
+        customer.ListOfPeriods == null ||
+        Object.hasOwn(customer, "ListOfPeriods")
+    ) {
+        return [];
+    }
 
-  return dashdivide(customer.ListOfPeriods);
+    return dashdivide(customer.ListOfPeriods);
 };
 
 export default extractDates;
